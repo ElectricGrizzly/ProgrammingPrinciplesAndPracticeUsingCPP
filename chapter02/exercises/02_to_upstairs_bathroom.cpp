6badce058fcba2a80b29965b770a11405ba2124e
Jeffrey Daye <jeffreydaye@protonmail.com>
2022-08-15 17:50:31 -0230

    exercise 2

---
+++ chapter02/exercises/02_to_upstairs_bathroom.cpp
@@ -0,0 +1,22 @@
+/*
+    Jeffrey D. Daye
+    August 15th, 2022
+    Chapter 2 - Exercise 2
+    Program that prints step-by-step instructions to upstairs bathroom from the front
+        entry of a house.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "********INSTRUCTIONS TO UPSTAIRS BATHROOM********" << std::endl;
+    std::cout << "1. After entering the front door, turn 90 degree to the left.\n"
+        << "2. Take two (2) steps forward through the porch door and turn 90 degrees right.\n"
+        << "3. Take five (5) steps forward and turn 90 degree right to face down the hallway.\n"
+        << "4. Take seven (7) steps forward down the hallway and turn left 90 degrees.\n"
+        << "5. Take two (2) steps forward.\n"
+        << "6. You are now in the bathroom.\n";
+
+    return 0;
+}

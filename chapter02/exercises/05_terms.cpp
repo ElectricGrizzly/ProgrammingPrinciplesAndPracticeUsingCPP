1ea37d18dc73fd69a620aae0f1d4d16b583fbb49
Jeffrey Daye <jeffreydaye@protonmail.com>
2022-08-15 20:51:51 -0230

    exercise 5

---
+++ chapter02/exercises/05_terms.cpp
@@ -0,0 +1,16 @@
+/*
+    Jeffrey D. Daye
+    August 15th, 2022
+    Chapter 2 - Exercise 5
+    Program that prints a message informing a viewer to check the terms folder.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "This exercise is to provide definitions for the terms at\n"
+        << "the end of each chapter. Please see the terms directory.\n";
+
+    return 0;
+}

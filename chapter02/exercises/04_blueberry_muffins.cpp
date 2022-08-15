6eefe40f9864aac957d638095def021461af1c17
Jeffrey Daye <jeffreydaye@protonmail.com>
2022-08-15 18:09:55 -0230

    exercise 4 incomplete

---
+++ chapter02/exercises/04_blueberry_muffins.cpp
@@ -0,0 +1,16 @@
+/*
+    Jeffrey D. Daye
+    August 15th, 2022
+    Chapter 2 - Exercise 4
+    Program that prints step-by-step instructions to make some delicious blueberry
+        muffins.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "********BLUEBERRY MUFFINS RECIPE********" << std::endl;
+
+    return 0;
+}

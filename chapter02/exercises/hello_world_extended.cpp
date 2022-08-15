80192c7c1c286d6fe85b544fb484760d96904de9
Jeffrey Daye <jeffreydaye@protonmail.com>
2022-08-14 22:16:52 -0230

    chapter 2 exercise 1

---
+++ chapter02/exercises/hello_world_extended.cpp
@@ -0,0 +1,16 @@
+/*
+    Jeffrey D. Daye
+    August 14th, 2022
+    Chapter 2 - Exercise 1
+    CLASSIC C++ Hello World (programming) with an extra line
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, programming!\n"
+        << "Here we go!\n";
+
+    return 0;
+}

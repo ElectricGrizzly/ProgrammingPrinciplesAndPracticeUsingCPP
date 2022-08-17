a0afe089964f464dea2e4eb3d2d8218fa6b44b81
Jeffrey Daye <jeffreydaye@protonmail.com>
2022-08-17 19:38:45 -0230

    Add while

--- chapter03/exercises/09_spelled_to_digit.cpp
+++ chapter03/exercises/09_spelled_to_digit.cpp
@@ -14,24 +14,25 @@ int main()
     
     std::cout << "Please spell out a number value (followed by 'enter'):" << std::endl;
     std::cin >> number;
-
-    if (number == "zero") {
+    while (std::cin >> number) {
+        if (number == "zero") {
         std::cout << 0 << std::endl;
-    }
-    else if (number == "one") {
-        std::cout << 1 << std::endl;
-    }
-    else if (number == "two") {
-        std::cout << 2 << std::endl;
-    }
-    else if (number == "three") {
-        std::cout << 3 << std::endl;
-    }
-    else if (number == "four") {
-        std::cout << 4 << std::endl;
-    }
-    else {
-        std::cout << "Not a number I know!" << std::endl;
+        }
+        else if (number == "one") {
+            std::cout << 1 << std::endl;
+        }
+        else if (number == "two") {
+            std::cout << 2 << std::endl;
+        }
+        else if (number == "three") {
+            std::cout << 3 << std::endl;
+        }
+        else if (number == "four") {
+            std::cout << 4 << std::endl;
+        }
+        else {
+            std::cout << "Not a number I know!" << std::endl;
+        }
     }
 
     return 0;

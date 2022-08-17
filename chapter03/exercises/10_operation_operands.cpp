650c594d5b53584b07777cf373aaff5f62b5f390
Jeffrey Daye <jeffreydaye@protonmail.com>
2022-08-17 19:48:26 -0230

    chapter 3 exercise 10

---
+++ chapter03/exercises/10_operation_operands.cpp
@@ -0,0 +1,42 @@
+/*
+    Jeffrey D. Daye
+    August 17th, 2022
+    Chapter 3 - Exercise 10
+    Perfomrs an operation based on user input of an operator followed by two operand values.
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string operation;
+    double operand1;
+    double operand2;
+
+    while(std::cin >> operation >> operand1 >> operand2) {
+        if (operation == "+" || operation == "plus") {
+            std::cout << operand1 << " + " << operand2 << " = " << operand1 + operand2 << std::endl;
+        }
+        else if (operation == "-" || operation == "minus") {
+            std::cout << operand1 << " - " << operand2 << " = " << operand1 - operand2 << std::endl;
+        }
+        else if (operation == "*" || operation == "mul") {
+            std::cout << operand1 << " * " << operand2 << " = " << operand1 * operand2 << std::endl;  
+        }
+        else if (operation == "/" || operation == "div") {
+            if (operand2 == 0) {
+                std::cout << "Cannot divide by 0!" << std::endl;
+            }
+            else {
+                std::cout << operand1 << " / " << operand2 << " = " << operand1 / operand2 << std::endl;
+            }   
+        }
+        else {
+            std::cout << "No valid operation specified. Try again." << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}

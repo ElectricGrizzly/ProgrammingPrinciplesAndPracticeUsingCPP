c009f86a3dcc6fd901512ddef963231cf2a39b02
Jeffrey Daye <jeffreydaye@protonmail.com>
2022-08-21 18:31:10 -0230

    chapter 4 exercise 4 complete

--- chapter04/exercises/04_guessing_game.cpp
+++ chapter04/exercises/04_guessing_game.cpp
@@ -2,7 +2,7 @@
     Jeffrey D. Daye
     August 21st, 2022
     Chapter 4 - Exercise 4
-    A numbers guessing game that guesses a number between 1 and 100 in seven guesses.
+    A numbers guessing game that guesses a number between 1 and 100 (non-inclusive) in seven guesses.
 */
 
 #include <iostream>
@@ -15,17 +15,17 @@ int main()
     int min = 1;
     char response = ' ';
     int guesses = 0;
-    int guess_point = max - min == 1 ? max : (max - min)/2 + min;
+    int guess_point = 0;
 
     std::cout << "Think of a number between " << min << " and " << max << std::endl;
 
-    while(min != max) {
-        int guess_point = max - min == 1 ? max : (max - min)/2 + min;
-        std::cout << guesses + 1 << ". Is your number less than " << guess_point << "? (y/n):" << std::endl;
+    while(max - min != 1) {
+        guess_point = (max - min)/2 + min;
+        std::cout << guesses + 1 << ". Is your number less than or equal to " << guess_point << "? (y/n):" << std::endl;
         std::cin >> response;
 
         if (response == 'y' || response == 'Y') {
-            max = guess_point - 1;
+            max = guess_point;
         }
         else if (response == 'n' || response == 'N') {
             min = guess_point;
@@ -37,8 +37,10 @@ int main()
         guesses++;
         std::cout << std::endl;
     }
-         
+
     std::cout << "Your number must be " << max << "!" << std::endl;
+
+    std::cout << "I figured it out in " << guesses << " guesses!" << std::endl;
 
     return 0;
 }

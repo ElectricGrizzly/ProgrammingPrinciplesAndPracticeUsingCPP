dc084d2c7c75c0a27e671c7ae628083657aa8dea
Jeffrey Daye <jeffreydaye@protonmail.com>
2022-08-31 23:22:01 -0230

    chapter 4 exercise 20

---
+++ chapter04/exercises/20_name_scores_score_by_name.cpp
@@ -0,0 +1,40 @@
+/*
+    Jeffrey D. Daye
+    August 31st, 2022
+    Chapter 4 - Exercise 20
+    Take entry of name and value pairs and output the results. Print out results by name
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+int main()
+{
+    std::string name = "";
+    int score = 0;
+    std::unordered_map<std::string, int> names_scores;
+
+    std::cout << "Enter a name followed by a score: " << std::endl;
+    while(std::cin >> name >> score && (name != "NoName" || score != 0)) {
+        if(names_scores.count(name) == 0) {
+            names_scores[name] = score;
+        }
+        else {
+            std::cout << "That name has already been entered. Terminating!" << std::endl;
+            break;
+        }
+    }
+
+    std::cout << "Enter the name of the score you'd like to find (enter '|' to terminate): " << std::endl;
+    while(std::cin >> name && name != "|") {
+        if(names_scores.count(name) == 0) {
+            std::cout << "Name not found!" << std::endl;
+        }
+        else {
+            std::cout << "Score: " << names_scores[name] << std::endl;
+        }
+    }
+
+    return 0;
+}

f58124fed82d2eabb44bf8f0344f487ebc2b8e6a
Jeffrey Daye <jeffreydaye@protonmail.com>
2022-08-31 23:16:26 -0230

    chapter 4 exercise 19

---
+++ chapter04/exercises/19_name_scores.cpp
@@ -0,0 +1,34 @@
+/*
+    Jeffrey D. Daye
+    August 31st, 2022
+    Chapter 4 - Exercise 19
+    Take entry of name and value pairs and output the results.
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+int main()
+{
+    std::string name = "";
+    int score = 0;
+    std::unordered_map<std::string, int> names_scores;
+
+    std::cout << "Enter a name followed by a score: " << std::endl;
+    while(std::cin >> name >> score && (name != "NoName" || score != 0)) {
+        if(names_scores.count(name) == 0) {
+            names_scores[name] = score;
+        }
+        else {
+            std::cout << "That name has already been entered. Terminating!" << std::endl;
+            break;
+        }
+    }
+
+    for (auto name: names_scores) {
+        std::cout << "Name: " << name.first << "\tScore: " << name.second << std::endl;
+    }
+
+    return 0;
+}
